b9bbf504fe82d1c2106a4b211480a1adfea9bb4d
MikuGhoul <mikughoull@gmail.com>
2017-11-02 10:08:10 +0800

    Two Sum

---
+++ C++/Two Sum/Two Sum.cpp
@@ -0,0 +1,33 @@
+// 236 ms submission
+/*
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		unordered_map<int, int> index;
+		for (int i = 0; i < nums.size(); ++i) {
+			auto search = index.find(target - nums[i]);
+			if (search != index.end())
+				return vector<int> {search->second, i};
+			index[nums[i]] = i;
+		}
+	}
+};
+
+*/
+
+
+// 3 ms submission
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> index;
+        for (int i = 0; i < nums.size(); ++i) {
+            auto search = index.find(target - nums[i]);
+            if (search != index.end())
+                return vector<int> {search->second, i};
+            index[nums[i]] = i;
+        }
+    }
+};
